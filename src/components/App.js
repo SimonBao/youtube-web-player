91ba713d714013a133a19ae632610cdd33b72a23
SimonBao <Simonbao.sb@gmail.com>
2021-09-19 17:22:44 +0100

    Youtube video search with basic styling

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import youtube from "../components/api/youtube";
+import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+
+class App extends Component {
+  state = { videos: [], selectedVideo: null };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: { q: term },
+    });
+
+    this.setState({ videos: response.data.items });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+        {this.state.selectedVideo && (
+          <VideoDetail video={this.state.selectedVideo} />
+        )}
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
