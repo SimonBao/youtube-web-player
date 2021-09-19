d7486d11ab0b0cd6140493ae86950a937a0ed652
SimonBao <Simonbao.sb@gmail.com>
2021-09-19 17:44:29 +0100

    change layout with semantic ui grids

--- src/components/App.js
+++ src/components/App.js
@@ -23,13 +23,21 @@ class App extends Component {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
-        {this.state.selectedVideo && (
-          <VideoDetail video={this.state.selectedVideo} />
-        )}
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelect={this.onVideoSelect}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              {this.state.selectedVideo && (
+                <VideoDetail video={this.state.selectedVideo} />
+              )}
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
